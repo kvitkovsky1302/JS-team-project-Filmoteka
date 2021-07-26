a9f202adca5953f25c4b93b16b37baef85530f0f
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-07-26 13:40:28 +0300

    add modal with students

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './sass/main.scss';
+
+
+const refs = {
+    studentGoit: document.querySelector('.dev__link'),
+    backdrop: document.querySelector('[data-modal]'),
+    modal: document.querySelector('.modal-students'),
+
+}
+
+refs.studentGoit.addEventListener('click', onToggleModal);
+refs.backdrop.addEventListener('click', onToggleModal);
+window.addEventListener('keydown', onCloseModal);
+refs.modal.addEventListener('click', stopPropagation);
+
+
+function onToggleModal(e) {
+    refs.backdrop.classList.toggle('is-hidden');
+}
+
+function onCloseModal(e) {
+    if (e.key === "Escape") {
+        refs.backdrop.classList.toggle('is-hidden');
+    }
+}
+
+function stopPropagation(e) {
+    e.stopPropagation();
+}
