756dd07253c4ff611322d8ff9b6432cd88ace168
Vladislav Kulachko <vladcoool@gmail.com>
2021-07-28 06:51:53 +0300

    завершает код модалки и хранилища
    1. перенос парса модалки в файл main-markup-maker, там же код записи в local storage.  2. считывание local storage и парс библиотеки пользователя в library-markup-maker.

--- src/library.js
+++ src/library.js
@@ -3,5 +3,5 @@ import './sass/main.scss';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/students';
 import './js/spinner';
-import './js/markup-modal';
+import './js/library-markup-maker';
 import './js/up-button';
